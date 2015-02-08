ed5ca966448ad8a40bc0e1eca4c99c4d239237ce
saasbook <saasbook@saasbook.(none)>
2015-02-08 10:39:21 -0400

    modified

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,6 +4,56 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  
+  attr_accessor :word , :guesses, :wrong_guesses 
+
+  def initialize(new_word)
+    @word = new_word 
+    @guesses = '' 
+    @wrong_guesses= ''
+  end   
+  
+  def guess(new_guess)
+     if new_guess == '' or new_guess =='%' or new_guess == nil  
+         raise ArgumentError
+     end 
+ 
+     if (@guesses + @wrong_guesses).include?(new_guess) 
+         return false  
+     elsif @word.include?(new_guess)  
+         @guesses +=new_guess 
+     else 
+         @wrong_guesses +=new_guess
+     end 
+     true   
+  end 
+
+  def word_with_guesses 
+     sofar = '' 
+     @word.chars do |letter| 
+        if @guesses.include?(letter) 
+            sofar += letter 
+        else 
+            sofar += '-' 
+        end 
+     end 
+     return sofar 
+  end 
+
+  def check_win_or_lose 
+      if @word.length == @guesses.length
+          :win  
+      elsif @wrong_guesses.length >= 7 
+          :lose 
+      else 
+          :play 
+      end 
+  end 
+
+            
+     
+    
+  
 
   def self.get_random_word
     require 'uri'
